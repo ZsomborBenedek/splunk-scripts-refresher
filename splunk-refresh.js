b36385c33be61949d588c6a70ab8efc3a4fa5cc8
ZsomborBenedek <44943540+ZsomborBenedek@users.noreply.github.com>
2021-09-27 12:46:06 +0200

    Create splunk-refresh.js

---
+++ splunk-refresh.js
@@ -0,0 +1,67 @@
+javascript: (() => {
+    let progressBar = document.getElementById('progress-bar');
+    if (progressBar === null) {
+        progressBar = document.createElement('div');
+        progressBar.id = 'progress-bar';
+        progressBar.style.display = 'flex';
+        progressBar.style.alignContent = 'center';
+        progressBar.style.justifyContent = 'center';
+        progressBar.style.color = 'white';
+        progressBar.style.fontSize = '1rem';
+        progressBar.style.height = '0';
+        progressBar.style.overflow = 'hidden';
+        progressBar.style.position = 'sticky';
+        progressBar.style.top = '0';
+        progressBar.style.zIndex = '10000';
+        progressBar.style.width = '100%';
+        progressBar.style.transition = 'all .5s ease-in-out';
+        document.body.insertBefore(progressBar, document.body.firstChild);
+    }
+
+    const load_scripts = (delay, message) => new Promise(resolve => {
+        progressBar.textContent = message;
+        progressBar.style.backgroundColor = 'green';
+        progressBar.style.height = `${progressBar.scrollHeight}px`;
+        setTimeout(resolve, delay);
+    });
+
+    const error_scripts = message => {
+        progressBar.textContent = message;
+        progressBar.style.backgroundColor = 'red';
+        setTimeout(() => {
+            progressBar.style.height = '0';
+            setTimeout(() => {
+                progressBar.remove();
+            }, 500);
+        }, 2500);
+    }
+
+    const success_scripts = message => {
+        progressBar.textContent = message;
+        progressBar.style.backgroundColor = 'green';
+        setTimeout(() => {
+            progressBar.style.height = '0';
+            setTimeout(() => {
+                progressBar.remove();
+            }, 500);
+        }, 2500);
+    }
+
+    const url = window.location.origin + '/en-US/debug/refresh';
+
+    load_scripts(500, 'Reloading scripts...').then(
+        fetch(url)
+            .then(res => {
+                return res.status >= 400 ? error_scripts(`${window.location.origin} is not a splunk site!`) : res.text();
+            })
+            .then(html => {
+                let parser = new DOMParser();
+                let page = parser.parseFromString(html, "text/html");
+                page.forms[0].submit();
+                success_scripts('Scripts reloaded!')
+            })
+            .catch(err => {
+                error_scripts(window.location.origin + ' is not a splunk site!');
+            })
+    );
+})();
